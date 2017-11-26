bcc074d54656b365bf7f0a7bcc05bbe7ea4ea3b8
Karura28 <vanessatrevizo10@gmail.com>
2017-11-25 23:23:18 -0700

    Texturas

--- www/js/scene.js
+++ www/js/scene.js
@@ -24,68 +24,95 @@ function generarcolor() {
 	return color;
 }
 //Pino
+var coneTexture =  new THREE.ImageUtils.loadTexture("public/pino.jpg");
 var geometryCone = new THREE.ConeGeometry( 20, 30, 4, 65 );
-var materialP = new THREE.MeshLambertMaterial({color: 0x0F8A5F}); 
+var materialP = new THREE.MeshLambertMaterial({map:coneTexture}); 
 var cone = new THREE.Mesh(geometryCone, materialP);
 cone.position.set(1,25,0);
 scene.add( cone );
-
+//pino2
+var cone1Texture =  new THREE.ImageUtils.loadTexture("public/pino.jpg");
 var geometryCone1 = new THREE.ConeGeometry( 30, 30, 4, 70 );
-var materialP = new THREE.MeshLambertMaterial({color: 0x0F8A5F}); 
+var materialP = new THREE.MeshLambertMaterial({map:cone1Texture}); 
 var cone1 = new THREE.Mesh(geometryCone1, materialP);
 cone1.position.set(1,8,0);
 scene.add( cone1 );
-
+//pino3
+var cone2Texture =  new THREE.ImageUtils.loadTexture("public/pino.jpg");
 var geometryCone2 = new THREE.ConeGeometry( 40, 30, 4, 70 );
-var materialP = new THREE.MeshLambertMaterial({color: 0x0F8A5F}); 
+var materialP = new THREE.MeshLambertMaterial({map: cone2Texture}); 
 var cone2 = new THREE.Mesh(geometryCone2, materialP);
 cone2.position.set(1,-10,0);
 scene.add( cone2 );
-
+//base
+var CubeTexture = new THREE.ImageUtils.loadTexture("public/madera.jpg");
 var geometryCube = new THREE.CubeGeometry(15,25,15);
-var materialC = new THREE.MeshLambertMaterial({color:0x3B170B });
+var materialC =  new THREE.MeshBasicMaterial({map: CubeTexture});
 var cube = new THREE.Mesh(geometryCube, materialC);
 cube.position.set(1,-25,0);
 scene.add( cube );
+//colores de las esferas
+var EsferaTexture = new THREE.ImageUtils.loadTexture("public/rojo.jpg");
+var EsferaTexture1 = new THREE.ImageUtils.loadTexture("public/azul.jpeg");
+var EsferaTexture2 = new THREE.ImageUtils.loadTexture("public/dorado.jpg");
+var EsferaTexture3 = new THREE.ImageUtils.loadTexture("public/morado.jpg");
+var EsferaTexture4 = new THREE.ImageUtils.loadTexture("public/verde.jpg");
 
 //Esferas 1er piso
 var geometrySphere = new THREE.SphereGeometry( 2, 32, 32 );
-var material = new THREE.MeshBasicMaterial( {color:  generarcolor()} );
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture} );
 var sphere = new THREE.Mesh( geometrySphere, material );
 sphere.position.set(2,5,7);
 cone.add(sphere);
 
-var material = new THREE.MeshBasicMaterial( {color:  generarcolor()} );
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture2} );
 var sphere2 = new THREE.Mesh( geometrySphere, material );
-sphere2.position.set(-15,-5,-1);
+sphere2.position.set(3,-20,3);
 cone.add(sphere2);
 
-var material = new THREE.MeshBasicMaterial( {color:  generarcolor()} );
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture3} );
 var sphere3 = new THREE.Mesh( geometrySphere, material );
 sphere3.position.set(15,-12,6);
 cone.add(sphere3);
 
-//Esferas 2do piso
-var material = new THREE.MeshBasicMaterial( {color:  generarcolor()} );
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture1} );
 var sphere4 = new THREE.Mesh( geometrySphere, material );
-sphere4.position.set(17,-11,10);
-cone1.add(sphere4);
+sphere4.position.set(-15,-12,6);
+cone.add(sphere4);
 
-var material = new THREE.MeshBasicMaterial( {color:   generarcolor()});
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture4} );
 var sphere5 = new THREE.Mesh( geometrySphere, material );
-sphere5.position.set(-25,-15,-6);
-cone1.add(sphere5);
+sphere5.position.set(-5,-5,-10);
+cone.add(sphere5);
 
-/*var material = new THREE.MeshBasicMaterial( {color:  0xffffff} );
+//Esferas 2do piso
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture2} );
 var sphere6 = new THREE.Mesh( geometrySphere, material );
-sphere6.position.set(16,10,12);
-cone.add(sphere6);*/
+sphere6.position.set(17,-11,10);
+cone1.add(sphere6);
 
-/*var sphere7 = new THREE.Mesh( geometrySphere, material );
-sphere.position.set(17,10,10);
-cone.add(sphere7);*/
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture1});
+var sphere7 = new THREE.Mesh( geometrySphere, material );
+sphere7.position.set(-25,-15,-6);
+cone1.add(sphere7);
 
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture});
+var sphere8 = new THREE.Mesh( geometrySphere, material );
+sphere8.position.set(17,-11,-11);
+cone1.add(sphere8);
 
+ /*/Esferas tercer piso
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture1} );
+var sphere9 = new THREE.Mesh( geometrySphere, material );
+sphere6.position.set(-20,-50,-20);
+cone.add(sphere9);
+
+var material = new THREE.MeshBasicMaterial( {map:EsferaTexture2} );
+var sphere10 = new THREE.Mesh( geometrySphere, material );
+sphere10.position.set(17,10,10);
+cone.add(sphere10);
+
+/*/
 //Estrella
 /*var starTexture = new THREE.ImageUtils.loadTexture("public/star.png");
 var geometryScuare = new THREE.Geometry();
